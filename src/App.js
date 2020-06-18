1cf6f534411d6ea0fd15f3f0bd6137d8cd149433
Vincent Bilotta <vbilotta@gmail.com>
2020-06-18 15:13:47 -0400

    setup api call; successfully receiving data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useFetch } from "./hooks/useFetch";
 
 function App() {
+  const sandboxUrl =
+    "https://fhir-open.sandboxcerner.com/dstu2/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/Patient/1316024";
+  var obj = {
+    method: "GET",
+    headers: {
+      Accept: "application/json+fhir",
+    },
+  };
+  const [response, loading, hasError] = useFetch(sandboxUrl, obj);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {loading ? (
+        <div>Loading...</div>
+      ) : hasError ? (
+        <div>Error occured.</div>
+      ) : (
+        <div>{response?.resourceType}</div>
+      )}
+    </>
   );
 }
 
